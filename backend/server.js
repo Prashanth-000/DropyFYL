79d89bd25f8aec43282fa6ea4e0ab46332c08b27
prashanth <151809864+Prashanth-000@users.noreply.github.com.>
2025-11-05 18:39:32 +0530

    deployment fix

--- backend/server.js
+++ backend/server.js
@@ -30,11 +30,9 @@ const io = new Server(server, {
 });
 
 app.use(cors({
-  origin: FRONTEND_URL,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
+  origin: "*"
 }));
+
 
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
